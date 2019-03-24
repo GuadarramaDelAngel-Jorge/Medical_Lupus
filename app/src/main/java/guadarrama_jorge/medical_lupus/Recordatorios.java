2c67bfeef67a4a1840ba9f541893d86c0e6b303d
SrGaara <nikel_black@hotmail.com>
2019-03-23 21:45:11 -0600

    ayuda complete

---
+++ app/src/main/java/guadarrama_jorge/medical_lupus/Recordatorios.java
@@ -0,0 +1,13 @@
+package guadarrama_jorge.medical_lupus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recordatorios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recordatorios);
+    }
+}
