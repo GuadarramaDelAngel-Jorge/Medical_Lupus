b8e78b813d517bd9b8d94ea65aa3145d252c2c40
SrGaara <nikel_black@hotmail.com>
2019-03-25 22:03:49 -0600

    navegacion

--- app/src/main/java/guadarrama_jorge/medical_lupus/AnalisisDiario.java
+++ app/src/main/java/guadarrama_jorge/medical_lupus/AnalisisDiario.java
@@ -1,6 +1,39 @@
 package guadarrama_jorge.medical_lupus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AnalisisDiario extends AppCompatActivity {
+
+    public void ViewPPrincipal(View view)
+    {
+        Intent myIntent = new Intent(view.getContext(), Principal.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void ViewPAnalisis(View view)
+    {
+        Intent myIntent = new Intent(view.getContext(), AnalisisDiario.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void ViewPResultados(View view)
+    {
+        Intent myIntent = new Intent(view.getContext(), Resultados.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void ViewPRecordatorios(View view)
+    {
+        Intent myIntent = new Intent(view.getContext(), Recordatorios.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    public void ViewPAyuda(View view)
+    {
+        Intent myIntent = new Intent(view.getContext(), AyudaAjustes.class);
+        startActivityForResult(myIntent, 0);
+    }
 }
+
